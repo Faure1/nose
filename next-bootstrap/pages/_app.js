95792febf7c51e898387c9f4c25b6aabf630049a
santiagofaginoli <102366056+santiagofaginoli@users.noreply.github.com>
2022-06-10 14:15:21 -0300

    Update _app.js

--- next-bootstrap/pages/_app.js
+++ next-bootstrap/pages/_app.js
@@ -1,4 +1,9 @@
-import '../styles/index.scss'
+import '../styles/globals.scss'
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
