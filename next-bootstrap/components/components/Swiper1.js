21508ffd202b820af5dd3ae24b3f1ea0ba224402
Faure1 <lautarofaure@iresm.edu.ar>
2022-06-11 22:16:58 -0300

    22.16

---
+++ next-bootstrap/components/components/Swiper1.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const Swiper1 = ({children}) => {
+    return ( 
+        <main>
+            <h1>
+                primer swiper
+            </h1>
+
+
+
+
+            {children}
+
+        </main>
+     );
+}
+ 
+export default Swiper1 ;
